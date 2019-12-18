faae3d153340a709966c63b5a2fa494ead566553
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-18 12:41:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(name)
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
